83e929f5fa85ba2a63266c405fc23d8e8c9484e7
Cecilie94 <144773754+Cecilie94@users.noreply.github.com>
2024-02-25 20:26:10 +0100

    added firebase key

--- src/main.js
+++ src/main.js
@@ -1,5 +1,23 @@
-import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
+import { createApp } from "vue";
+import "./style.css";
+import App from "./App.vue";
 
-createApp(App).mount('#app')
+createApp(App).mount("#app");
+
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyC6CPtLfPS4TNP-zoQKSPdN1JkYpybXIbQ",
+  authDomain: "fir-3e800.firebaseapp.com",
+  projectId: "fir-3e800",
+  storageBucket: "fir-3e800.appspot.com",
+  messagingSenderId: "271968500928",
+  appId: "1:271968500928:web:cb16bb7409ae6379d64125",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
